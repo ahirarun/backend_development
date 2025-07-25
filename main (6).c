14e97df4a5e720ec596df15b5c3a0077f35e76a5
ahir24 <ahiratyadav269@gmail.com>
2025-07-25 17:29:14 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+      printf("\n \t your name : vishal");
+      printf("\n \t your birth date:23\7\2004");
+      printf("\n \t your age : 21");
+      printf("\n \t your address : nagvadar");
+      
+      getch();
+    
+}
